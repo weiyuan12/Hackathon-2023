9931dfd4f2db1c5d8fc5e7779393c8a77965f074
weiyuan12 <weiyuan.tan0@gmail.com>
2023-01-16 15:50:41 +0800

    2

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
 bot_token = "5976242202:AAHjE6IDSL5wSnsJHJy7pFoQ_8QV3s364pQ"
 bot_user_name = "t.me/HDBHelper_bot"
-URL = "https://hdbhelper.herokuapp.com/setwebhook"
+URL = "https://hdbhelper.herokuapp.com"
